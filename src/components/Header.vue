5e9170483a71f88c9443d84c90d3e4f99c6cdd8e
gnd-vn <victor.hugo.negosek.de.oliveira@ipt.fraunhofer.de>
2022-04-12 14:45:41 +0200

    Routing Implementation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -15,6 +16,15 @@ export default {
   name: "Header",
   props: { title: String, showAddTask: Boolean },
   components: { Button },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 </script>
 
